abb566713428e638454aa825e5840ecc43f16fb0
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2023-03-04 16:07:04 +0200

    Initial commit

--- Assets/Scripts/HorizontalSpawn.cs
+++ Assets/Scripts/HorizontalSpawn.cs
@@ -3,32 +3,42 @@ using UnityEngine;
 
 public class HorizontalSpawn : MonoBehaviour
 {
-    [SerializeField] private GameObject Prefab;
-    float x = -8.693f;
-    float y = 27.07f;
-    bool isTrue = true;
-    private void Update()
+    int RandNum;
+    [SerializeField] private GameObject Prefab0;
+    [SerializeField] private GameObject Prefab1;
+    [SerializeField] private GameObject Prefab2;
+    [SerializeField] private GameObject Prefab3;
+    [SerializeField] private GameObject Prefab4;
+    public float space = 7;
+    float y = -4f;
+
+    private void Start()
     {
-        if(isTrue)
+        for (int i = 0; i <= 10; i++)
         {
-            StartCoroutine(Timer());
-            isTrue= false;
+            RandNum = Random.Range(0, 5);
+            switch (RandNum)
+            {
+                case 0:
+                    Instantiate(Prefab0, new Vector2(0, y += space), Quaternion.identity);
+                    break;
+                case 1:
+                    Instantiate(Prefab1, new Vector2(0, y += space), Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(Prefab2, new Vector2(0, y += space), Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(Prefab3, new Vector2(0, y += space), Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(Prefab4, new Vector2(0, y += space), Quaternion.identity);
+                    break;
+            }
         }
     }
 
 
 
-    public void OnSpawnAPrefab()
-    {
-        Instantiate(Prefab, new Vector2(x,y),Quaternion.identity);
-    }
 
-
-    IEnumerator Timer()
-    {
-        yield return new WaitForSecondsRealtime(3);
-        OnSpawnAPrefab();
-        isTrue= true;
-    }
-    
 }
