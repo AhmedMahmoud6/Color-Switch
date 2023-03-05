99153e61e00640425871d30c48fd40e63b0e9a8f
Ahmed Mahmoud <ahmedmahmoudd626@gmail.com>
2023-03-05 21:51:48 +0200

    Initial commit

--- Assets/Scripts/HorizontalSpawn.cs
+++ Assets/Scripts/HorizontalSpawn.cs
@@ -9,36 +9,47 @@ public class HorizontalSpawn : MonoBehaviour
     [SerializeField] private GameObject Prefab2;
     [SerializeField] private GameObject Prefab3;
     [SerializeField] private GameObject Prefab4;
+    [SerializeField] private GameObject Prefab5;
+    [SerializeField] private GameObject ColorChanger;
     public float space = 7;
     float y = -4f;
+    float yChanger = -7.5f;
 
     private void Start()
     {
-        for (int i = 0; i <= 10; i++)
+        for (int i = 0; i <= 20; i++)
         {
-            RandNum = Random.Range(0, 5);
+            Instantiate(ColorChanger, new Vector2(0, yChanger += space), Quaternion.identity);
+
+            RandNum = Random.Range(0, 6);
             switch (RandNum)
             {
                 case 0:
                     Instantiate(Prefab0, new Vector2(0, y += space), Quaternion.identity);
+                    Prefab0.SetActive(true);
                     break;
                 case 1:
                     Instantiate(Prefab1, new Vector2(0, y += space), Quaternion.identity);
+                    Prefab1.SetActive(true);
                     break;
                 case 2:
                     Instantiate(Prefab2, new Vector2(0, y += space), Quaternion.identity);
+                    Prefab2.SetActive(true);
                     break;
                 case 3:
                     Instantiate(Prefab3, new Vector2(0, y += space), Quaternion.identity);
+                    Prefab3.SetActive(true);
                     break;
                 case 4:
                     Instantiate(Prefab4, new Vector2(0, y += space), Quaternion.identity);
+                    Prefab4.SetActive(true);
+                    break;
+                case 5:
+                    Instantiate(Prefab5, new Vector2(-1.32f, y += space), Quaternion.identity);
+                    Prefab5.SetActive(true);
                     break;
             }
         }
     }
-
-
-
 
 }
---
+++ Assets/Scripts/StartButton.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartButton : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            SceneManager.LoadScene("StartMenu");
+        }
+    }
+}
